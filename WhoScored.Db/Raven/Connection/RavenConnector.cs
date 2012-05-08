c9bc3d741dd3416c1f84f5cd9723dc99ecb31309
Erikas Pliauksta At Work <erikaspl@gmail.com>
2012-05-08 18:04:39 +0100

    added ravendb packages.

---
+++ WhoScored.Db/Raven/Connection/RavenConnector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using Raven.Client.Document;
+
+namespace WhoScored.Db.Connection
+{
+    public class RavenConnector
+    {
+        public static DocumentStore CreateConnection()
+        {
+            var documentStore = new DocumentStore {ConnectionStringName = "RavenDbConncetion"};
+            documentStore.Initialize();
+            return documentStore;
+        }
+    }
+}
